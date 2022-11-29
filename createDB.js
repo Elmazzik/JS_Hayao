771c3b6f78b465ae76186e9582289ba35ef2428b
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2022-11-30 00:52:45 +0300

    «Express»

--- createDB.js
+++ createDB.js
@@ -1,15 +1,16 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
-const uri = "mongodb://localhost:27017/"
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("threecats");
+var database = client.db("hayao");
 database.dropDatabase()
-database = client.db("threecats");
-const cats = database.collection("cats");
-const result = await cats.insertOne({name:"Барсик"});
+database = client.db("hayao");
+const cats = database.collection("anime");
+const result = await cats.insertOne({name:"Хаяо"});
 console.log(`${result} documents were inserted`);
 } finally {
 await client.close();
