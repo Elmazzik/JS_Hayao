59b7b60406fe0c65c10a286ddad99504f24cf0b2
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2022-11-30 22:00:12 +0300

    «Express»

--- routes/hayao.js
+++ routes/hayao.js
@@ -9,6 +9,19 @@ router.get('/', function(req, res, next) {
 /* Страница аниме */
 router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
-  });
+});
+
+/* Страница героев */
+router.get('/:nick', function(req, res, next) {
+    Cat.findOne({nick:req.params.nick}, function(err,cat){
+        if(err) return next(err)
+        if(!cat) return next(new Error("Нет такого аниме на этой странице"))
+        res.render('hayao', {
+            title: hayao.title,
+            picture: hayao.avatar,
+            desc: hayao.desc
+        })
+    })
+})
 
 module.exports = router;
