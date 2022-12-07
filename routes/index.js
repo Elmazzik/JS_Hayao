0e7104f4e6ce67dbcfc7eaf316c67287c4f83ff8
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2022-12-07 21:14:01 +0300

    «Express»

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  City.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
+  Hayao.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
   })
 });
 
