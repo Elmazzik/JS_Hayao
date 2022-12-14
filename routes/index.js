870af542283f6c9e0fdccbb8bcb1211d8a0db943
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2022-12-14 21:07:45 +0300

    «Express»

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Hayao = require("../models/hayao").Hayao
+var User = require("./../models/user").User
 
 
 /* GET home page. */
@@ -14,9 +15,28 @@ res.render('logreg',{title: 'Вход'});
 });
 
 /* POST login/registration page. */
-router.post('/logreg', function(req, res, next) {
-    
-
+/* POST login/registration page. */
+router.post('/logreg', function (req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({ username: username }, function (err, user) {
+    if (err) return next(err)
+    if (user) {
+      if (user.checkPassword(password)) {
+        req.session.user = user._id
+        res.redirect('/')
+      } else {
+        res.render('logreg', { title: 'Вход' })
+      }
+    } else {
+      var user = new User({ username: username, password: password })
+      user.save(function (err, user) {
+        if (err) return next(err)
+        req.session.user = user._id
+        res.redirect('/')
+      })
+    }
+  })
 });
 
 
